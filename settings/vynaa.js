6a6a5df40a36ec98373e7d9799fe3aa04bf0e2ce
Vynaa <118973846+VynaaValerie@users.noreply.github.com>
2024-09-02 17:46:28 +0700

    Update vynaa.js

--- settings/vynaa.js
+++ settings/vynaa.js
@@ -1,1 +1,54 @@
-se
+const settings = {
+    headerImageDesktop: "https://telegra.ph/file/880aaef1599624e37ac22.jpg", // Background image for desktop
+    headerImageMobile: "https://telegra.ph/file/880aaef1599624e37ac22.jpg", // Background image for mobile
+    profilePicture: "https://telegra.ph/file/c839697bc25cf4ca75def.jpg",
+    profileName: "@biolink_studio_code",
+    socialMedia: {
+        x: {
+            url: "https://discord.gg/c6wYDZfhhc",
+            followers: "1.2k"
+        },
+        facebook: {
+            url: "https://instagram.com/vynaa_valerie",
+            followers: "1.7k"
+        },
+        youtube: {
+            url: "https://youtube.com/@VynaaChan",
+            followers: "3.6k"
+        },
+        instagram: {
+            url: "https://instagram.com/vynaa_valerie",
+            followers: "1.7k"
+        },
+        tiktok: {
+            url: "https://www.tiktok.com/@yourusername",
+            followers: "250k"
+        },
+        telegram: {
+            url: "https://t.me/yourusername",
+            followers: "1k"
+        }
+    },
+    albums: [
+        {
+            title: "Album 1",
+            description: "My first album",
+            coverImage: "https://telegra.ph/file/4bc5749bbdf19606b8b7a.jpg",
+            link: "https://open.spotify.com/album/abc"
+        },
+        {
+            title: "Album 2",
+            description: "My second album",
+            coverImage: "https://telegra.ph/file/31c26d4a7ec6dbaf6b891.jpg",
+            link: "https://open.spotify.com/album/def"
+        },
+        {
+            title: "Album 3",
+            description: "My third album",
+            coverImage: "https://telegra.ph/file/9578806504a73366fc6b3.jpg",
+            link: "https://open.spotify.com/album/ghi"
+        }
+    ],
+    whatsappChatUrl: "https://wa.me/6282389924037" // WhatsApp chat URL
+};
+          
